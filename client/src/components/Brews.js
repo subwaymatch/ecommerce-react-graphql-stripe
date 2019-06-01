56e6b471490e51694f6bdf65f0d685984725aded
Ye Joo Park <subwaymatch@gmail.com>
2019-06-01 02:46:49 -0700

    Add brand page and display brews

--- client/src/components/Brews.js
+++ client/src/components/Brews.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Strapi from "strapi-sdk-javascript/build/main";
+import { Box, Heading, Text, Image, Card, Button } from "gestalt";
+
 const apiUrl = process.env.API_URL || "http://localhost:1337";
 const strapi = new Strapi(apiUrl);
 
 class Brews extends React.Component {
+  state = {
+    brews: [],
+    brand: ""
+  };
   async componentDidMount() {
     try {
       const response = await strapi.request("POST", "/graphql", {
@@ -15,6 +21,7 @@ class Brews extends React.Component {
               brews {
                 _id
                 name
+                description
                 image {
                   url
                 }
@@ -25,16 +32,83 @@ class Brews extends React.Component {
         }
       });
 
-      console.log(response);
+      this.setState({
+        brand: response.data.brand.name,
+        brews: response.data.brand.brews
+      });
     } catch (err) {
       console.error(err);
     }
   }
   render() {
+    const { brand, brews } = this.state;
     return (
-      <div>
-        <div>Brews</div>
-      </div>
+      <Box
+        marginTop={4}
+        display="flex"
+        justifyContent="center"
+        alignItems="start"
+      >
+        {/* Brews Section */}
+        <Box display="flex" direction="column" alignItems="center">
+          {/* Brews Heading */}
+          <Box margin={2}>
+            <Heading color="orchid">{brand}</Heading>
+          </Box>
+
+          {/* Brews */}
+          <Box
+            dangerouslySetInlineStyle={{
+              __style: {
+                backgroundColor: "#bdcdd9"
+              }
+            }}
+            wrap
+            shape="rounded"
+            display="flex"
+            justifyContent="center"
+            padding={4}
+          >
+            {brews.map(brew => (
+              <Box paddingY={4} width={210} margin={2} key={brew._id}>
+                <Card
+                  image={
+                    <Box height={250} width={200}>
+                      <Image
+                        fit="cover"
+                        src={`${apiUrl}${brew.image.url}`}
+                        alt={brew.name}
+                        naturalHeight={1}
+                        naturalWidth={1}
+                      />
+                    </Box>
+                  }
+                >
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    direction="column"
+                  >
+                    <Box margin={2}>
+                      <Text bold size="xl">
+                        {brew.name}
+                      </Text>
+                    </Box>
+                    <Text>{brew.description}</Text>
+                    <Text color="orchid">${brew.price}</Text>
+                    <Box marginTop={2}>
+                      <Text bold size="xl">
+                        <Button color="blue" text="Add to Cart" />
+                      </Text>
+                    </Box>
+                  </Box>
+                </Card>
+              </Box>
+            ))}
+          </Box>
+        </Box>
+      </Box>
     );
   }
 }
