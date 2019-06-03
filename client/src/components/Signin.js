97c2df34f7b7479298cb25ffa1af46d7f22c38ce
Ye Joo Park <subwaymatch@gmail.com>
2019-06-03 02:42:44 -0700

    Markup for the checkout form

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Box, Button, Heading, Text, TextField } from "gestalt";
+import { Container, Box, Button, Heading, TextField } from "gestalt";
 import { setToken } from "../utils";
 import ToastMessage from "./ToastMessage";
 import Strapi from "strapi-sdk-javascript/build/main";
