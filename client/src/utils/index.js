212c27e966c45145000034faccde956cb7dcb766
Ye Joo Park <subwaymatch@gmail.com>
2019-06-03 04:53:22 -0700

    Add missing utils and ToastMessage component files

---
+++ client/src/utils/index.js
@@ -0,0 +1,50 @@
+const CART_KEY = "BREW_HAHA_CART";
+const TOKEN_KEY = "BREW_HAHA_TOKEN_KEY";
+
+export const calculatePrice = items => {
+  return `${items
+    .reduce((acc, item) => acc + item.price * item.quantity, 0)
+    .toFixed(2)}`;
+};
+
+/* Cart */
+export const setCart = (value, cartKey = CART_KEY) => {
+  if (localStorage) {
+    localStorage.setItem(cartKey, JSON.stringify(value));
+  }
+};
+
+export const getCart = (cartKey = CART_KEY) => {
+  if (localStorage && localStorage.getItem(cartKey)) {
+    return JSON.parse(localStorage.getItem(cartKey));
+  }
+
+  return [];
+};
+
+export const clearCart = (cartKey = CART_KEY) => {
+  if (localStorage) {
+    localStorage.removeItem(cartKey);
+  }
+};
+
+/* Authentication */
+export const setToken = (value, tokenKey = TOKEN_KEY) => {
+  if (localStorage) {
+    localStorage.setItem(tokenKey, JSON.stringify(value));
+  }
+};
+
+export const getToken = (tokenKey = TOKEN_KEY) => {
+  if (localStorage && localStorage.getItem(tokenKey)) {
+    return JSON.parse(localStorage.getItem(tokenKey));
+  }
+
+  return null;
+};
+
+export const clearToken = (tokenKey = TOKEN_KEY) => {
+  if (localStorage) {
+    localStorage.removeItem(tokenKey);
+  }
+};
