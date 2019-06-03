fa14a5d96dfb67beafbbf5316e9e2c07707c0868
Ye Joo Park <subwaymatch@gmail.com>
2019-06-02 21:49:55 -0700

    Create private route HOC

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 import "gestalt/dist/gestalt.css";
+
+import { getToken } from "./utils/index";
 
 import App from "./components/App";
 import Navbar from "./components/Navbar";
@@ -12,6 +19,21 @@ import Checkout from "./components/Checkout";
 import registerServiceWorker from "./registerServiceWorker";
 import Brews from "./components/Brews";
 
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      getToken() !== null ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{ pathname: "/signin", state: { from: props.location } }}
+        />
+      )
+    }
+  />
+);
+
 const Root = () => (
   <Router>
     <React.Fragment>
@@ -20,7 +42,7 @@ const Root = () => (
         <Route component={App} exact path="/" />
         <Route component={Signin} path="/signin" />
         <Route component={Signup} path="/signup" />
-        <Route component={Checkout} path="/checkout" />
+        <PrivateRoute component={Checkout} path="/checkout" />
         <Route component={Brews} path="/:brandId" />
       </Switch>
     </React.Fragment>
